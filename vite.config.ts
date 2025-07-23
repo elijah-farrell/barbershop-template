749bf8bfa7f21522ba6260b6bc58f859c6171e4f
Elijah Farrell <farrelea@sunypoly.edu>
2025-07-23 01:19:50 -0400

    add logo and custom images, fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/barbershop-template/', // GitHub Pages deployment path
+  base: mode === 'production' ? '/barbershop-template/' : '/',
   server: {
     port: 3000,
     open: true
   }
-}) 
+})) 
